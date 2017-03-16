eb464bf6d5e5b24f9ff9252273962576a618af0f
Louis Bettens <louis@bettens.org>
2017-03-16 17:46:55 +0100

    switch to unittest

--- test_hypothesis.py
+++ test_hypothesis.py
@@ -1,15 +1,17 @@
 from hypothesis import given
 from strategies import *
+import unittest
 
 import mpack
 
 
-@given(all_types)
-def test_pack_unpack(x):
-    packed_obj, obj = x
-    unpacker = mpack.Unpacker()
-    print("unpacking %r..." % packed_obj)
-    unpacked_obj, n = unpacker(packed_obj)
-    assert n == len(packed_obj)
-    print("comparing...")
-    assert unpacked_obj == obj
+class TestMpack(unittest.TestCase):
+    @given(all_types)
+    def test_pack_unpack(self, x):
+        packed_obj, obj = x
+        unpacker = mpack.Unpacker()
+        print("unpacking %r..." % packed_obj)
+        unpacked_obj, n = unpacker(packed_obj)
+        self.assertEqual(n, len(packed_obj))
+        print("comparing...")
+        self.assertEqual(unpacked_obj, obj)
