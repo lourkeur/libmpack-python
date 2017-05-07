a5686ce3779a6ee976023aab93299d2d0a9d8c93
Louis Bettens <louis@bettens.org>
2017-05-07 17:24:37 +0200

    remove verbose debug print statements

--- test_hypothesis.py
+++ test_hypothesis.py
@@ -10,10 +10,8 @@ class TestMpack(unittest.TestCase):
     def test_pack_unpack(self, x):
         packed_obj, obj = x
         unpacker = mpack.Unpacker()
-        print("unpacking %r..." % packed_obj)
         unpacked_obj, n = unpacker(packed_obj)
         self.assertEqual(n, len(packed_obj))
-        print("comparing...")
         self.assertEqual(unpacked_obj, obj)
 
     @unittest.skip("hangs and requires the process to be killed manually")
