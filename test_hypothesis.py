f922fa1d050cabb6b56d80fe6b7bb8f3f2a3fcd5
Louis Bettens <louis@bettens.org>
2017-05-07 18:05:09 +0200

    add roundtrip test

--- test_hypothesis.py
+++ test_hypothesis.py
@@ -9,10 +9,18 @@ class TestMpack(unittest.TestCase):
     @given(strategies.everything())
     def test_pack_unpack(self, x):
         packed_obj, obj = x
-        unpacker = mpack.Unpacker()
-        unpacked_obj, n = unpacker(packed_obj)
-        self.assertEqual(n, len(packed_obj))
-        self.assertEqual(unpacked_obj, obj)
+        with self.subTest(packed_obj=packed_obj, obj=obj):
+            with self.subTest("unpack(packed_obj) == obj"):
+                unpack = mpack.Unpacker()
+                unpacked_obj, n = unpack(packed_obj)
+                self.assertEqual(n, len(packed_obj))
+                self.assertEqual(unpacked_obj, obj)
+            with self.subTest("unpack(pack(unpack(packed_obj))) == obj"):
+                pack, unpack = mpack.Packer(), mpack.Unpacker()
+                packed_obj = pack(unpacked_obj)
+                unpacked_obj, n = unpack(packed_obj)
+                self.assertEqual(n, len(packed_obj))
+                self.assertEqual(unpacked_obj, obj)
 
     @unittest.skip("hangs and requires the process to be killed manually")
     def test_unpacking_c1(self):
